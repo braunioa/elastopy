87b12753ccfef76ea268cfda30697e971d7df03b
nasseralkmim <nasser.alkmim@gmail.com>
2017-06-30 15:48:39 -0300

    added plane strain into material module

--- elastopy/material.py
+++ elastopy/material.py
@@ -1,11 +1,32 @@
-"""Module that contains material properties in a class
-
-"""
+"""Module that contains material properties in a class"""
 
 
 class Material(object):
     """Instanciate a material object
 
+    Args:
+        case (str): which case is the constitutive model
+
+    Attributes:
+        mat_dic keywords (dict): dictionary with surface label and 
+          material paramenter value.
+
+          example:
+
+                Material(E={9: 1000})
+
+          then, the keyword `E` is the attribute self.E with value
+          {9: 1000} in which 9 is the surface label and 1000 is the 
+          E value at that surface
+
     """
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
+    def __init__(self, case='stress', **mat_dic):
+        self.__dict__.update(mat_dic)
+
+        # convert from plane stress to plane strain
+        if case is 'strain':
+            for surf, E in self.E.items():
+                self.E[surf] = self.E[surf]/(1 - self.nu[surf]**2)
+            for surf, nu in self.E.items():
+                self.nu[surf] = self.nu[surf]/(1 - self.nu[surf])
+
