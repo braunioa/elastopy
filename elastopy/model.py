bb94206d1724370b05ad5d9368cab0d8f15fe2f6
nasseralkmim <nasser.alkmim@gmail.com>
2017-07-10 16:15:49 -0300

    added class to create level set mask array and modified model class

--- elastopy/model.py
+++ elastopy/model.py
@@ -5,12 +5,57 @@ structure model.
 The structure model has all attributes from the mesh object.
 
 """
+import numpy as np
+import levelset
+from elastopy.xfem.zerolevelset import Create
 
 
 class Build(object):
     """Build the model object
 
+    Args:
+        mesh (object): object containing mesh data
+        zerolevelset (optional): mask array with -1, 1 or any other
+            value which zero level set defines discontinuity interface
+
+    Attributes:
+        mesh attributes
+        enriched_elements (list): elements that are enriched
+
     """
-    def __init__(self, mesh):
+    def __init__(self, mesh, zerolevelset=None):
         # copy attributes from mesh object
         self.__dict__ = mesh.__dict__.copy()
+
+        self.enriched_elements = []
+        if zerolevelset is not None:
+            # phi shape (nn, ) with signed distance value
+            phi = levelset.distance(zerolevelset.mask_ls,
+                                    zerolevelset.grid_x,
+                                    zerolevelset.grid_y,
+                                    self.XYZ)
+
+            for e, conn in enumerate(self.CONN):
+                # check if element is enriched or not
+                if np.all(phi[conn] < 0) or np.all(
+                        phi[conn] > 0):
+                    pass
+                else:
+                    self.enriched_elements.append(e)
+
+
+if __name__ == '__main__':
+    class Mesh():
+        pass
+    mesh = Mesh()
+    mesh.XYZ = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
+    mesh.CONN = np.array([[0, 1, 2, 3]])
+
+    # center in (1, 1) and radius .2
+    def func(x, y):
+        return (x - 1)**2 + (y - 1)**2 - .2**2
+    z_ls = Create(func, [0, 1], [0, 1], num_div=50)
+
+    model = Build(mesh, zerolevelset=z_ls)
+    # print(model.enriched_elements)
+    # [0]
---
+++ elastopy/xfem/zerolevelset.py
@@ -0,0 +1,43 @@
+"""creates an object with the zero level set definition"""
+import numpy as np
+
+
+class Create(object):
+    """creates the zero level set object with its attributes
+
+    Args:
+        function (func(x, y)): function that defines the level set
+            the zero level set defines the discontinuity interface
+        x_domain (list): domian defined by (x_domain[0], x_domain[1])
+        y_domain (list): same as x
+        num_div (float, optional): number of division for defining the
+            level set, the greater the value more precisa the interface
+            will be defined
+
+    Attributes:
+        grid_x (numpy array): 2d array shape (num_div, num_div)
+            with grid value for x direction
+        grid_y (numpy array): same as x for y direction
+        mask_ls (numpy array): 2d array shape (num_div, num_div) with
+            -1 and 1 where the points between these two values define the
+            discontinuity interface.
+    """
+    def __init__(self, function, x_domain, y_domain, num_div=50):
+        self.grid_x, self.grid_y = np.meshgrid(np.linspace(x_domain[0],
+                                                           x_domain[1],
+                                                           num_div),
+                                               np.linspace(y_domain[0],
+                                                           y_domain[1],
+                                                           num_div))
+        ls = function(self.grid_x, self.grid_y)
+        self.mask_ls = ls/abs(ls)
+
+
+if __name__ == '__main__':
+    def func(x, y):
+        return (x - 2)**2 + (y - 2)**2 - 1.8**2
+    z_ls = Create(func, [0, 2], [0, 2], num_div=3)
+    print(z_ls.mask_ls)
+    # [[ 1.  1.  1.]
+    #  [ 1. -1. -1.]
+    #  [ 1. -1. -1.]]
